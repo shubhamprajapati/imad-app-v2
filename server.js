6697a482a4a4ada7d1d078a8f09d92df3578dce0
shubhamprajapati <cse15bcs1925@gmail.com>
2017-02-04 02:03:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/', function (req, res) {
 app.get('/article-one', function(req, res){
     res.sendFile('Article one requeseted')
 });
-app.get('/article-one', function(req, res){
-    res.sendFile('Article one requeseted')
-});app.get('/article-one', function(req, res){
-    res.sendFile('Article one requeseted')
+app.get('/article-two', function(req, res){
+    res.sendFile('Article two requeseted')
+});app.get('/article-three', function(req, res){
+    res.sendFile('Article three requeseted')
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
